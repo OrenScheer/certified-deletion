7c333e9f980bf24c072215c44ac291a08a27ee97
Oren Scheer <oren.rs723@gmail.com>
2022-06-07 10:04:00 -0400

    Bring in relevant cleanup from simplified-scheme branch

--- utils.py
+++ utils.py
@@ -1,6 +1,4 @@
 import numpy as np
-from qiskit import QuantumCircuit, execute, transpile
-from qiskit_parameters import backend, optimization_level
 
 
 def random_bit_string(length: int) -> str:
@@ -19,20 +17,6 @@ def random_bit() -> int:
 def random_int(lower_bound_inclusive: int, upper_bound_inclusive: int) -> int:
     # Returns a random number in [lower_bound_inclusive, upper_bound_inclusive]
     return np.random.randint(lower_bound_inclusive, upper_bound_inclusive + 1)
-
-
-def run_and_measure(circuit: QuantumCircuit) -> str:
-    print(circuit.draw())
-    transpiled_circuit = transpile(
-        circuit, backend=backend, optimization_level=0)
-    print(transpiled_circuit.draw())
-    # result = execute(transpiled_circuit, backend=backend,
-    #                  optimization_level=optimization_level, shots=1).result()
-    result = backend.run(transpiled_circuit, shots=1).result()
-    counts = result.get_counts()
-    print(counts)
-    # Reverse the string since the most significant qubit is at the 0th index of the measurement string
-    return list(counts.keys())[0][::-1]
 
 
 def xor(*bit_strings: str) -> str:
--- verification_circuit.py
+++ verification_circuit.py
@@ -1,26 +1,37 @@
+from typing import Tuple
 from states import Key
 from utils import xor, hamming_weight
 
 
-def verify(key: Key, certificate: str) -> bool:
+def verify(key: Key, certificate: str) -> Tuple[bool, int]:
     certificate_restricted_i_bar = "".join(
         [certificate[i] for i, ch in enumerate(key.theta) if ch == "1"])
     # print(f"Expected certificate of deletion: {key.r_restricted_i_bar}")
     # print(f"Received certificate of deletion: {certificate_restricted_i_bar}")
     # TODO: accept the certificate even if it's off by some bits, dependent on a parameter delta
-    return hamming_weight(xor(key.r_restricted_i_bar, certificate_restricted_i_bar)) == 0
+    hamming_distance = hamming_weight(
+        xor(key.r_restricted_i_bar, certificate_restricted_i_bar))
+    return hamming_distance == 0, hamming_distance
 
 
 def verify_deletion_counts(certificates: dict[str, int], key: Key) -> None:
     accepted_count = 0
     rejected_count = 0
+    rejected_string_distances = {}
     for certificate, count in certificates.items():
-        if verify(key, certificate):
+        is_exact_match, distance = verify(key, certificate)
+        if is_exact_match:
             accepted_count += count
         else:
             rejected_count += count
+            rejected_string_distances[distance] = rejected_string_distances.get(
+                distance, 0) + count
     total_count = accepted_count + rejected_count
     print(
         f"Accepted proof of deletion: {accepted_count}/{total_count} ({(accepted_count / total_count)*100}%)")
     print(
         f"Rejected proof of deletion: {rejected_count}/{total_count} ({(rejected_count / total_count)*100}%)")
+    if rejected_string_distances:
+        print(f"Out of the {rejected_count} rejected certificates, the following are the counts of the Hamming distances between the received certificate and the expected certificate:")
+        for distance, count in sorted(rejected_string_distances.items()):
+            print(f"Hamming distance {distance}: {count}")
