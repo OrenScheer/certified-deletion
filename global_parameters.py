9c8b18e161bd55f9a453aee49568589ad9ed362b
Oren Scheer <oren.rs723@gmail.com>
2022-06-07 10:26:51 -0400

    Add comments for parameters

--- global_parameters.py
+++ global_parameters.py
@@ -11,19 +11,25 @@ class GlobalParameters:
     # TODO: implement the below functions
 
     def calculate_n(self):
+        # Length of the message
         return 6
 
     def calculate_m(self):
+        # Total number of qubits
         return 15
 
     def calculate_k(self):
+        # Number of qubits used for deletion
         return 7
 
     def calculate_s(self):
+        # Number of qubits used for a one-time pad for the message
         return self.m - self.k
 
     def calculate_tau(self):
+        # Length of error correction hash
         return 5
 
     def calculate_mu(self):
+        # Length of error syndromes
         return 5
